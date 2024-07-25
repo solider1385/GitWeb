617fa8f54a98a6368a79da04298e3c6c59a4b68e
solider1385 <109354084+solider1385@users.noreply.github.com>
2024-07-25 19:07:04 +0330

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+let number = document.getElementById('number');
+    let circle = document.getElementById('circle');
+    let count = 0;
+
+    circle.addEventListener('click', function() {
+      count++;
+      number.innerHTML = count + " D";
+});
